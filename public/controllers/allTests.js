5f00d7906f412a3fd7f9bae356c20ac79e45a4e2
strakz <strakytto@gmail.com>
2016-05-05 03:46:38 +0200

    commit 5.5

---
+++ public/controllers/allTests.js
@@ -0,0 +1,26 @@
+angular.module('SymText')
+    .controller('AllTestListCtrl', function($scope, $http) {
+
+        $scope.tests = [];
+        $scope.showList = false;
+
+
+        $http.get('/api/alltests').success(function (data) {
+            // now we have all our movies and can add them
+            $scope.tests = data;
+            console.log(data.length);
+            console.log($scope.tests.length);
+
+            if ($scope.tests.length > 0) {
+                $scope.showList = true;
+            } else {
+                $scope.showList = false;
+            }
+        });
+
+        $scope.edit=function(tests){
+            //console.log(student.student._id);
+            console.log(tests.test._id)
+
+        }
+    });
